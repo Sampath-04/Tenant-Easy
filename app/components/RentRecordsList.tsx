676be43710e9ae5eae533811b3db802e654464db
Sampath-04 <sampathkaali002@gmail.com>
2025-09-17 20:12:55 +0530

    updated production link for payment request

--- app/components/RentRecordsList.tsx
+++ app/components/RentRecordsList.tsx
@@ -52,8 +52,10 @@ export default function RentRecordsList({
         setPaymentFormOpen(true);
     };
 
+    const domain = process.env.NEXT_PUBLIC_NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://easy-tenant-front-end.vercel.app';
+
     const getWhatsAppMessage = (rent: any) => {
-    const paymentLink = `http://localhost:3000/tenant-payment?rentRecordId=${rent._id}`;
+    const paymentLink = `${domain}/tenant-payment?rentRecordId=${rent._id}`;
     const nextMonthPeriod = getNextMonthRentPeriodFromRecord(rent);
     const message = 
 `Hi ${rent.tenant.tenantName}, 
